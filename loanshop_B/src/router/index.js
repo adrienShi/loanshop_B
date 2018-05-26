5b5ecb1719a69c7ac5456918f49891da6e5ea6b3
adrien <2876425803@qq.com>
2018-05-26 19:59:51 +0800

    b

---
+++ loanshop_B/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import loanList from '@/components/loanList'
+import register from '@/components/register'
+import his from '@/components/his'
+import evaluate from '@/components/evaluate' 
+import evaluateDetails from '@/components/evaluateDetails' 
+import agreement from '@/components/agreement' 
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'loanList',
+      component: loanList
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register
+    },
+    {
+      path: '/his',
+      name: 'his',
+      component: his
+    },
+    {
+      path: '/evaluate',
+      name: 'evaluate',
+      component: evaluate
+    },
+    {
+      path: '/evaluateDetails',
+      name: 'evaluateDetails',
+      component: evaluateDetails
+    },
+    {
+      path: '/agreement',
+      name: 'agreement',
+      component: agreement
+    }
+  ]
+})
